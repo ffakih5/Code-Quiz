43704a5b3ec34c476cbd5d7ff6ae038aa54e5514
Fatima Fakih <fatima.fakih@hotmail.com.au>
2020-12-24 11:22:54 +1100

    list of variables

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 function launchQuiz(){
-        let remainingTime=0;
+        var remainingTime=0;
 }
+
+
+    var startButton= document.querySelector("#start-button");
+    var remainingTime= document.querySelector("#remaining-time");
+    var finalScore= document.querySelector("#final-score");
+    var questionCount= questions.length;
+    var containerLanding= document.querySelector("#container-landing");
+    var containerQuiz= document.querySelector("#container-quiz");
+    var containerFinal= document.querySelector("#container-final");
+    var submitButton= document.querySelector("#submit-initials");
+    var hsButton= document.querySelector("#HS-button");
+    var containerHighscore= document.querySelector("#container-highscore")
+    var highScores = [];
+
+    
+
+
+
+
+
     console.log("Quiz");
     var quizQuestions = [
        {
