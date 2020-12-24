652f64b97fa64a985b7e3896678f5776849033b0
Fatima Fakih <fatima.fakih@hotmail.com.au>
2020-12-24 21:41:08 +1100

    function startQuiz

--- script.js
+++ script.js
@@ -2,32 +2,41 @@
     var remainingTime = document.querySelector("#time");
     var quizAnswers = document.querySelector("#answers");
     var submitBtn = document.querySelector("#submit");
-    var startBtn = document.querySelector("#quiz-start");
+    var startBtn = document.querySelector("#begin");
     var inputInitials = document.querySelector("#initials");
   
    
-    var questionCount = 0
+    var questionCount = 0;
     var time = questions.length * 20;
     var timeR; 
 
-    function launchQuiz() {
-    var remainingTime=0;
-}
+    //function launchQuiz() {
+   // var remainingTime=0;
+//}
     
-    function start(){
-        var questionDiv = document.getElementById('quiz');
-        questionDiv.innerHTML = showQuestion(0)
+    function startQuiz(){
+
+        var quizStart= document.getElementById("quiz-start");
+        quizStart.setAttribute("class", "hide");
+
+        quizQuestions.removeAttribute("class");
+
+        timeR = setInterval(clockTick, 1000);
+
+        timeR.textContent = time;
+
+        getQuestion();
     
     }
-    var startQuiz = document.querySelector("#start-button");
-    startQuiz.addEventListener("click", start-button) 
+   // var startQuiz = document.querySelector("#start-button");
+   // startQuiz.addEventListener("click", start-button) 
     
 
-    console.log(quizQuestions);
+    //console.log(quizQuestions);
     
 
-    function showQuestion (idx) {
-        return `
+    //function showQuestion (idx) {
+        //return `
             <h1> ${ quizQuestions[i].question}</h1>
             <ul>
             <li> ${ quizQuestions[i].answers[0]}</li>
@@ -36,4 +45,4 @@
             <li> ${ quizQuestions[i].answers[3]}</li>
             </ul> 
         `
-    }
+    //}
