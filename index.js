db59e829279fd3f25b8a4650032c014000a41da5
Fatima Fakih <fatima.fakih@hotmail.com.au>
2020-12-24 11:32:52 +1100

    separating questions to separate file

--- index.js
+++ index.js
@@ -16,48 +16,6 @@ function launchQuiz(){
     var highScores = [];
 
     
-
-
-
-
-
-    console.log("Quiz");
-    var quizQuestions = [
-       {
-        "question" : "What does HTML stand for?" ,
-        "answers"  : [
-           "HumanT", 
-           "bla bla",
-           "easyto understand",
-           "HyperTextMarkupLanguage"
-        ],
-         "rightanswer" : 3
-    },
-    
-       {
-        "question" : "What does CSS stand for?" ,
-        "answers"  : [
-           "HumanT", 
-           "Cascading Style Sheets",
-           "easyto understand",
-           "HyperTextMarkupLanguage"
-        ],
-         "rightanswer" : 1
-       }
-    ]
-    function showQuestion (idx) {
-        return `
-            <h1> ${ quizQuestions[i].question}</h1>
-            <ul>
-            <li> ${ quizQuestions[i].answers[0]}</li>
-            <li> ${ quizQuestions[i].answers[1]}</li>
-            <li> ${ quizQuestions[i].answers[2]}</li>
-            <li> ${ quizQuestions[i].answers[3]}</li>
-            </ul> 
-        
-        
-        
-    }
     function start(){
         var questionDiv = document.getElementById('quiz');
         questionDiv.innerHTML = showQuestion(0)
