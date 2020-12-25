de4070861ea9c097a277d521218fa129f003bdc0
Fatima Fakih <fatima.fakih@hotmail.com.au>
2020-12-26 07:20:14 +1100

    display/clear scores

--- scores.js
+++ scores.js
@@ -1,3 +1,28 @@
-function printHS(){
+function printhigh-scores(){
+    var highScores = JSON.parse(window.localStorage.getItem("high-scores")) || [];
+
+    highScores.sort(function(a,b) {
+        return b.score-a.score;
+
+    });
+
+    highScores.forEach(function(score){ 
+        var liItem = document.createElement("li");
+        liItem.textContent = score.initials + " - " + score.score;
+
+        var olItem = document.getElementById("high-scores");
+        olItem.appendChild(liItem)
+
+
+
     
+    }
+    
+    }
+        
+    
+
+
+
+
 }
