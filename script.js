3f5c3b8091d7877b9ac1983541f0942867fcd403
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-01-10 13:37:16 +1100

    deleting comments for submission

--- script.js
+++ script.js
@@ -7,12 +7,8 @@ var inputInitials = document.querySelector("#initials");
 
 
 var questionCount = 0;
-var time = questions.length * 20; //from id
+var time = quizquestions.length * 20; 
 var timeR; 
-
-//function launchQuiz() {
-// var remainingTime=0;
-//}
 
 function startQuiz(){
 
@@ -133,34 +129,3 @@ startBtn.onclick = startQuiz;
 inputInitials.onkeyup = enterPressed;
 
 console.log(questionClick);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var startQuiz = document.querySelector("#start-button");
-// startQuiz.addEventListener("click", start-button) 
-
-
-//console.log(quizQuestions);
-
-
-//function showQuestion (idx) {
-    //return `
-        //<h1> ${ quizQuestions[i].question}</h1>
-        //<ul>
-        //<li> ${ quizQuestions[i].answers[0]}</li>
-       // <li> ${ quizQuestions[i].answers[1]}</li>
-        //<li> ${ quizQuestions[i].answers[2]}</li>
-       // <li> ${ quizQuestions[i].answers[3]}</li>
-        //</ul> 
-    //`
-//}
