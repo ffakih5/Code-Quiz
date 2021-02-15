5b0d3d972a69a244a8cce8b15307cf818e7f0380
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-02-15 15:27:52 +1100

    readded file with proper camelCase

---
+++ questionSet.js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+     question: "What does HTML stand for?" ,
+     answer: ["HumananiTy MagnosteeL", "Hyper Tastic Magic Lions", "Horton Tells Murtle Loo", "Hyper Text Markup Language",],
+     rightanswer: 3
+     },
+    {
+     question: "What does CSS stand for?" ,
+     answer: ["Content Capture Send", "Cascading Style Sheets", "easyto understand", "Code Circuit Capture"],
+     rightanswer: 1
+    },
+    {
+     question: "Which of these is a programming language?" , 
+     answer: ["JavaScript", "Pythagoras Theorum","RubyCon","CSS",],
+     rightanswer : 0
+    },
+    { 
+     question : "Where do you find the Document title, meta data, styles and links?" , 
+     answer  : ["<html>", "<a>", "<script>", "<head>",],
+     rightanswer : 3
+    },
+    {
+     question: "What is used to enable a button when clicked?", 
+     answer: ["addElementById", "onClick", "addEventListener", "function()",],
+     rightanswer : 1
+    },
+    {
+     question : "What are typically found in an array?",
+     answer  : ["Booleans", "Strings", "Numbers", "All of the above",],
+     rightanswer : 3
+    }
+ ];
+ 
